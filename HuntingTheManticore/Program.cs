3c25df8b5cbd0f29a12dc172772a7c4e08f8718e
packet-nova <ellis.derek@outlook.com>
2025-04-15 14:06:56 -0400

    added sounds and switch statements for more varied logging; including colored text for cannon type on round status

--- HuntingTheManticore/Program.cs
+++ HuntingTheManticore/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Title = "Hunting the Manticore";
+﻿using System;
+
+Console.Title = "Hunting the Manticore";
 Console.ForegroundColor = ConsoleColor.Cyan;
 
 int manticoreCurrentHealth = 10;
@@ -9,6 +11,8 @@ int gameRound = 1;
 int manticoreDistance = SetManticoreDistance();
 
 Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Player 2, it is your turn.");
 
 // Game start
 while (checkIfAlive(manticoreCurrentHealth) && checkIfAlive(cityCurrentHealth))
@@ -48,13 +52,16 @@ static int SetManticoreDistance()
 {
     while (true)
     {
-        Console.Write($"Player 1: Choose the distance from the city to deploy the Manticore (0-100): ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Player 1, it is your turn.");
+        Console.Write("Choose the distance from the city to deploy the Manticore (0-100): ");
         int distance = Convert.ToInt32(Console.ReadLine());
 
         if (distance >= 0 && distance <= 100)
         {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine($"The Manticore is deployed a distance of {distance}.");
-            Console.WriteLine($"Press any key to start game...");
+            Console.WriteLine($"Press a key to begin...");
             Console.ReadKey();
             return distance;
         }
@@ -70,12 +77,32 @@ void RoundStart()
     string cannonType = GetCannonType();
 
     Console.ForegroundColor = ConsoleColor.Cyan;
-    Console.WriteLine();
+    var previousColor = Console.ForegroundColor;
     Console.WriteLine("-------------------------------------------------------------------");
     Console.WriteLine("STATUS");
     Console.WriteLine($"Round: {gameRound}");
-    //Console.WriteLine($"City is Alive: {cityIsAlive}\t\t\tManticore is Alive: {manticoreIsAlive}"); // Used for debug
-    Console.WriteLine($"Cannon Type: {cannonType}");
+    //Console.WriteLine($"Cannon Type: {cannonType}");
+    Console.Write($"Cannon Type: ");
+    switch (cannonType)
+    {
+        case "Plasma Blast":
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(cannonType.ToUpper());
+            break;
+        case "Fire":
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(cannonType);
+            break;
+        case "Electric":
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(cannonType);
+            break;
+        default:
+            Console.ForegroundColor = previousColor;
+            Console.WriteLine(cannonType);
+            break;
+    }
+    Console.ForegroundColor = previousColor;
     Console.WriteLine($"City Health: {cityCurrentHealth}/{cityMaxHealth}\t\t\tManticore Health: {manticoreCurrentHealth}/{manticoreMaxHealth}");
     AttackManticore(cannonType);
     cityCurrentHealth -= 1;
@@ -171,21 +198,28 @@ void LogDamage(string cannonType, int cannonDamage)
     string damageLog = ($"{cannonType} cannon hits for {cannonDamage} damage.");
     if (cannonType == "Plasma Blast")
     {
+        Console.Beep(800, 50);
+        Console.Beep(1200, 50);
         Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine(damageLog.ToUpper());
     }
     else if (cannonType == "Fire")
     {
+        Console.Beep(800, 50);
+        Console.Beep(1200, 50);
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(damageLog);
     }
     else if (cannonType == "Electric")
     {
+        Console.Beep(800, 50);
+        Console.Beep(1200, 50);
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine(damageLog);
     }
     else
     {
+        Console.Beep(600, 50);
         Console.ForegroundColor = ConsoleColor.Gray;
         Console.WriteLine(damageLog);
     }
@@ -206,3 +240,11 @@ void CityWin()
     Console.Beep(1000, 200);
     Console.Beep(1200, 400);
 }
+
+//void ColoredText(string word, ConsoleColor color)
+//{
+//    var previousColor = Console.ForegroundColor;
+//    Console.ForegroundColor = color;
+//    Console.Write(word);
+//    Console.ForegroundColor = previousColor;
+//}
