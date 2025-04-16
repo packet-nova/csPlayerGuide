9dc01da049bdcee8031d38af4a77d71d62777514
packet-nova <ellis.derek@outlook.com>
2025-04-16 02:02:39 -0400

    update

---
+++ Level17/Program.cs
@@ -0,0 +1,14 @@
+﻿//CookFood(FoodType.Soup, Ingredient.Mushroom, Seasoning.Salty);
+
+
+CookFood(FoodType.Gumbo, Ingredient.Potato, Seasoning.Spicy);
+
+void CookFood(FoodType food, Ingredient ingredient, Seasoning seasoning)
+{
+    Console.WriteLine($"You cooked a {seasoning} {ingredient} {food}.");
+}
+
+
+enum FoodType { Soup, Stew, Gumbo };
+enum Ingredient { Mushroom, Chicken, Carrot, Potato };
+enum Seasoning { Spicy, Salty, Sweet};
