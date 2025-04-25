aa3abfb064ca2d6152a196088d4fac196f6acc34
packet-nova <ellis.derek@outlook.com>
2025-04-25 19:02:45 -0400

    update

--- Level24/TicTacToe/Program.cs
+++ Level24/TicTacToe/Program.cs
@@ -1,3 +1,18 @@
-﻿
+﻿// Have player pick a symbol (X or O)
+// Ask player where they want to place a mark
+// End game if all 9 squares are occupied with no winner (draw)
+// Render the game board after each player turn
+// Detect and display when a player has won
+// How to display player symbol? Ma
+// 
+
+public class Player
+{
+    char _playerSymbol { get; set }
+}
 
 
+public class BoardState
+{
+
+}
