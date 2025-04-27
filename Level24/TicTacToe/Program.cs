295f1f6896ed09127907835beae928881decd42d
packet-nova <ellis.derek@outlook.com>
2025-04-26 20:15:37 -0400

    update

--- Level24/TicTacToe/Program.cs
+++ Level24/TicTacToe/Program.cs
@@ -4,11 +4,20 @@
 // Render the game board after each player turn
 // Detect and display when a player has won
 // How to display player symbol? Ma
-// 
+// Player cannot place symbol in square if already occupied
+
+char[,] symbols = new char[3, 3] { { 'X', 'O', ' ' }, { 'X', 'O', ' ' }, { 'X', 'O', ' ' } };
+symbols[0, 0] = 'A';
+
+Console.WriteLine($" {symbols[0, 0]} | {symbols[0, 0]} | {symbols[0, 0]} ");
+Console.WriteLine("---+---+---");
+Console.WriteLine($" {symbols[0, 0]} | {symbols[0, 0]} | {symbols[0, 0]} ");
+Console.WriteLine("---+---+---");
+Console.WriteLine($" {symbols[0, 0]} | {symbols[0, 0]} | {symbols[0, 0]} ");
 
 public class Player
 {
-    char _playerSymbol { get; set }
+    char _playerSymbol { get; set; }
 }
 
 
