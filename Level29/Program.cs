8a76d13f3a542c491a56423d1ff69eaffef75fa0
packet-nova <ellis.derek@outlook.com>
2025-05-05 17:08:41 -0400

    Level 29 Records

--- Level29/Program.cs
+++ Level29/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Sword sword1 = new(Material.Iron, Gemstone.None, 100, 30);
+Sword sword2 = sword1 with { material = Material.Steel, gemstone = Gemstone.Emerald };
+Sword sword3 = sword1 with { material = Material.Binarium, gemstone = Gemstone.Bitstone };
+
+Console.WriteLine(sword1);
+Console.WriteLine(sword2);
+Console.WriteLine(sword3);
+
+public record Sword(Material material, Gemstone gemstone, int length, int crossguardWidth);
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium };
+public enum Gemstone { None, Emerald, Amber, Sapphire, Diamond, Bitstone };
