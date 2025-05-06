8b24861234bc412daefc922a321c17bd15b0e01a
packet-nova <ellis.derek@outlook.com>
2025-05-05 22:29:30 -0400

    update

--- Level29/Program.cs
+++ Level29/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-Sword sword1 = new(Material.Iron, Gemstone.None, 100, 30);
+﻿Sword sword1 = new(Material.Iron, Gemstone.None, 100, 30);
 Sword sword2 = sword1 with { material = Material.Steel, gemstone = Gemstone.Emerald };
 Sword sword3 = sword1 with { material = Material.Binarium, gemstone = Gemstone.Bitstone };
 
