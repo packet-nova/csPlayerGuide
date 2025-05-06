3a3f27af5a17185c506dad39873b726d39ff5374
packet-nova <ellis.derek@outlook.com>
2025-05-06 09:00:37 -0400

    updated IsAdjacent method in level28

--- Level28/Program.cs
+++ Level28/Program.cs
@@ -3,17 +3,8 @@ Coordinate b = new(4, 4);
 Coordinate c = new(5, 5);
 Coordinate d = new(3, 4);
 
-Console.WriteLine($"Coordinates are adjacent: {IsAdjacent(a, b)}"); // Returns False
-Console.WriteLine($"Coordinates are adjacent: {IsAdjacent(c, d)}"); // Returns True
-
-bool IsAdjacent(Coordinate coord1, Coordinate coord2)
-{
-    if (Math.Abs(coord1.Row - coord2.Row) == 1 || (Math.Abs(coord1.Column - coord2.Column) == 1))
-    {
-        return true;
-    }
-    return false;
-}
+Console.WriteLine($"Coordinates are adjacent: {Coordinate.IsAdjacent(a, b)}"); // Returns False
+Console.WriteLine($"Coordinates are adjacent: {Coordinate.IsAdjacent(c, d)}"); // Returns True
 
 public readonly struct Coordinate
 {
@@ -24,5 +15,9 @@ public readonly struct Coordinate
     {
         Row = row;
         Column = column;
+    }
+    public static bool IsAdjacent(Coordinate coord1, Coordinate coord2)
+    {
+        return (Math.Abs(coord1.Row - coord2.Row) == 1 || (Math.Abs(coord1.Column - coord2.Column) == 1));
     }
 }
