6022ff95faff275243abe7b89e5832e2bea599a9
packet-nova <ellis.derek@outlook.com>
2025-05-06 11:30:56 -0400

    level 30 complete

--- Level30/Program.cs
+++ Level30/Program.cs
@@ -1,1 +1,34 @@
 ﻿
+using System.Net.NetworkInformation;
+
+Sword sword = new();
+Bow bow = new();
+Axe axe = new();
+
+ColoredItem<Sword> coloredSword = new ColoredItem<Sword>(sword);
+ColoredItem<Bow> coloredBow = new ColoredItem<Bow>(bow);
+ColoredItem<Axe> coloredAxe = new ColoredItem<Axe>(axe);
+
+coloredSword.Display(ConsoleColor.Blue);
+coloredBow.Display(ConsoleColor.Red);
+coloredAxe.Display(ConsoleColor.Green);
+
+public class ColoredItem<TItem>
+{
+    public TItem Item;
+    public ColoredItem(TItem item)
+    {
+        Item = item;
+    }
+
+    public void Display(ConsoleColor consoleColor)
+    {
+        Console.ForegroundColor = consoleColor;
+        Console.WriteLine(Item);
+        Console.ResetColor();
+    }
+}
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
