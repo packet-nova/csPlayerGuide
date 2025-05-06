3df538f91746c35f3b49f2ae9cf59bac014705a0
packet-nova <ellis.derek@outlook.com>
2025-05-05 23:03:01 -0400

    update

--- Level29/Program.cs
+++ Level29/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Sword sword1 = new(Material.Iron, Gemstone.None, 100, 30);
+Sword sword2 = sword1 with { material = Material.Steel, gemstone = Gemstone.Emerald };
+Sword sword3 = sword1 with { material = Material.Binarium, gemstone = Gemstone.Bitstone };
+
+Console.WriteLine(sword1);
+Console.WriteLine(sword2);
+Console.WriteLine(sword3);
+
+public record Sword(Material material, Gemstone gemstone, int length, int crossguardWidth)
+{
+    public override string ToString()
+    {
+        if (gemstone != Gemstone.None)
+        {
+            return $"A sword made from {material.ToString().ToLower()} inlaid with pristine cut {gemstone.ToString().ToLower()}s and measures {length} with a crossguard width of {crossguardWidth}.";
+        }
+        return $"A sword made from {material.ToString().ToLower()} that measures {length} with a crossguard width of {crossguardWidth}.";
+    }
+}
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium };
+public enum Gemstone { None, Emerald, Amber, Sapphire, Diamond, Bitstone };
