ab2531b40bd2fc2475c81e4b3137eb27857404c9
packet-nova <ellis.derek@outlook.com>
2025-05-16 17:14:45 -0400

    123

--- Sandbox/Item.cs
+++ Sandbox/Item.cs
@@ -1,7 +1,8 @@
-﻿public class Item
-{
-    public string Name { get; protected set; }
-    public double Weight { get; protected set; }
-    public int Value { get; protected set; }
-    public Rarity Rarity { get; protected set; }
-}
+﻿//public class Item
+//{
+//    public string Name { get; protected set; }
+//    public double Weight { get; protected set; }
+//    public int Value { get; protected set; }
+//    public Rarity Rarity { get; protected set; }
+//}
+
