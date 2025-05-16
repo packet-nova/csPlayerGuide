310bd1d0b4fc9f8dfc939e6489f797f3c2a5eac7
packet-nova <ellis.derek@outlook.com>
2025-05-16 19:58:34 -0400

    Updated player Look(); to add new method instead

--- Level31/TheFountainOfObjects/Player.cs
+++ Level31/TheFountainOfObjects/Player.cs
@@ -18,61 +18,33 @@
 
     public void Look(Game game)
     {
-        Location northLocation = new(Location.x - 1, Location.y);
-        Location southLocation = new(Location.x + 1, Location.y);
-        Location eastLocation = new(Location.x, Location.y + 1);
-        Location westLocation = new(Location.x, Location.y - 1);
-
         Console.WriteLine("You spend a turn using heightenend sense.");
 
-        // Check to the North
-        if (!game.Map.IsOutOfBounds(northLocation.x, northLocation.y))
+        CheckDirection(game, Direction.North, new Location(Location.x - 1, Location.y));
+        CheckDirection(game, Direction.South, new Location(Location.x + 1, Location.y));
+        CheckDirection(game, Direction.East, new Location(Location.x, Location.y + 1));
+        CheckDirection(game, Direction.West, new Location(Location.x, Location.y - 1));
+    }
+
+    /// <summary>
+    /// Checks what's in a specific direction and reports it to the player.
+    /// </summary>
+    /// <param name="game">The current game state</param>
+    /// <param name="direction">The direction to check</param>
+    /// <param name="locationToCheck">The location coordinates to examine</param>
+    private void CheckDirection(Game game, Direction direction, Location locationToCheck)
+    {
+        if (!game.Map.IsOutOfBounds(locationToCheck.x, locationToCheck.y))
         {
-            Console.WriteLine($"To the North: {game.Map.GetRoomAt(northLocation)} - ");
-            if (northLocation.Equals(game.Maelstrom.Location))
+            Console.WriteLine($"To the {direction}: {game.Map.GetRoomAt(locationToCheck)} - ");
+
+            if (locationToCheck.Equals(game.Maelstrom.Location))
             {
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine(game.Maelstrom.SenseDescription);
                 Console.ResetColor();
             }
         }
-
-        // Check to the South
-        if (!game.Map.IsOutOfBounds(southLocation.x, southLocation.y))
-        {
-            Console.WriteLine($"To the South: {game.Map.GetRoomAt(southLocation)} - ");
-            if (southLocation.Equals(game.Maelstrom.Location))
-            {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine(game.Maelstrom.SenseDescription);
-                Console.ResetColor();
-            }
-        }
-
-        // Check to the East
-        if (!game.Map.IsOutOfBounds(eastLocation.x, eastLocation.y))
-        {
-            Console.WriteLine($"To the East: {game.Map.GetRoomAt(eastLocation)} - ");
-            if (eastLocation.Equals(game.Maelstrom.Location))
-            {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine(game.Maelstrom.SenseDescription);
-                Console.ResetColor();
-            }
-        }
-
-        // Check to the West
-        if (!game.Map.IsOutOfBounds(westLocation.x, westLocation.y))
-        { 
-            Console.WriteLine($"To the West: {game.Map.GetRoomAt(westLocation)} - ");
-            if (westLocation.Equals(game.Maelstrom.Location))
-            {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine(game.Maelstrom.SenseDescription);
-                Console.ResetColor();
-            }
-        }
-
     }
 
     public void Move(Direction direction, Map map)
