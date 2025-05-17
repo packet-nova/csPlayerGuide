e95cb3d1798b2a63151e38d0469462c29966aa85
packet-nova <ellis.derek@outlook.com>
2025-05-17 14:03:38 -0400

    level 35

--- Sandbox/Item.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Item
-{
-    public string Name { get; protected set; }
-    public double Weight { get; protected set; }
-    public int Value { get; protected set; }
-    public Rarity Rarity { get; protected set; }
-}
