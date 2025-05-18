b36c2ac06641225028cb69f053f62d33ffe746bd
packet-nova <ellis.derek@outlook.com>
2025-05-17 21:00:40 -0400

    level 36 progress

---
+++ Level36/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(Double(20));
+
+
+
+
+int SubtractOne(int number) => number - 1;
+int Double(int number) => number * 2;
+int AddOne(int number) => number + 1;
+
+ChangeArrayElements(new int[] { 1, 2, 3, 4, 5 }, AddOne);
+
+int[] ChangeArrayElements(int[] numbers, NumberDelegate operation) {}
+public delegate int NumberDelegate(int number);
+
+
