a1b289198b76fab1ea679514b72a2048bbd734f7
packet-nova <ellis.derek@outlook.com>
2025-05-21 17:00:53 -0400

    level 39 progress

--- Level39/Program.cs
+++ Level39/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿//Console.Write("What do you want me to tell you next time? ");
+//string? message = Console.ReadLine();
+//File.WriteAllText("Message.txt", message);
+
+//string previous = File.ReadAllText("Message.txt");
+//Console.WriteLine("Last time, you said this: " + previous);
+
+//Console.Write("What do you want me to tell you next time? ");
+//string? message = Console.ReadLine();
+//File.WriteAllText("Message.txt", message);
