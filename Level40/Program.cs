3f2d2be5d8d0c5203af56e76090abdc51336dd22
packet-nova <ellis.derek@outlook.com>
2025-05-24 22:42:23 -0400

    asdf

---
+++ Level40/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine((Arrowhead)(0));
+Console.WriteLine((Arrowhead)(1));
+Console.WriteLine((Arrowhead)(2));
+
+public class Arrow
+{
+    Arrowhead Arrowhead = Arrowhead.Flint;
+}
+
+
+enum Arrowhead { Flint, Bone, Poison }
