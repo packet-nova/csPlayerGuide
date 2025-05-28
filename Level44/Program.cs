bbe79ed47ad718825228f3cd556bfe7119f45615
packet-nova <ellis.derek@outlook.com>
2025-05-28 18:11:57 -0400

    level 44 start

---
+++ Level44/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Task<int> AddOnEuropa(int a, int b)
+{
+    Task<int> task = new Task<int>(() =>
+    {
+        Thread.Sleep(3000);
+        return a + b;
+    });
+    task.Start();
+    return task;
+}
+*/
+
+Task<int> AddOnEuropa(int a, int b)
+{
+    return Task.Run(() =>
+    {
+        Thread.Sleep(3000);
+        return a + b;
+    });
+}
+
