79909516578046a1b1cd483af157efd7afafc608
packet-nova <ellis.derek@outlook.com>
2025-05-29 00:31:08 -0400

    44 progress

--- Level44/Program.cs
+++ Level44/Program.cs
@@ -11,6 +11,14 @@ Task<int> AddOnEuropa(int a, int b)
 }
 */
 
+
+Task<int> additionTask = AddOnEuropa(2, 3);
+Task addAndDisplay = additionTask.ContinueWith(t => Console.WriteLine(t.Result));
+
+additionTask.Wait();
+int result = additionTask.Result;
+Console.WriteLine(result);
+
 Task<int> AddOnEuropa(int a, int b)
 {
     return Task.Run(() =>
