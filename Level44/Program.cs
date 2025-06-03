6979a73ac67c5358917536bbbbea05a8a4402472
packet-nova <ellis.derek@outlook.com>
2025-06-03 14:33:50 -0400

    level 44 complete

--- Level44/Program.cs
+++ Level44/Program.cs
@@ -42,15 +42,17 @@ several seconds, and my math indicates that a 10-letter word may take nearly two
 Display the time elapsed (Level 32)
  */
 
-DateTime programStart= DateTime.Now;
-Console.WriteLine($"Total Attempts: {(RandomlyCreate("bananas")):N0}");
 
+Console.Write("Enter a word (6 or less characters): ");
+string userWord = Console.ReadLine().ToLower().Trim();
+await RandomlyRecreateAsync(userWord);
 
-int RandomlyCreate(string word)
+int RandomlyRecreate(string word)
 {
     int attempts = 0;
-    word = word.ToLower().Trim();
     string randomWord;
+
+    word = word.ToLower().Trim();
     Random random = new Random();
 
     do
@@ -62,10 +64,39 @@ int RandomlyCreate(string word)
             chars[i] = (char)('a' + random.Next(26));
         }
         randomWord = new string(chars);
-        //Console.WriteLine(randomWord);
         attempts++;
     }
-
     while (randomWord != word);
+
     return attempts;
+}
+
+Task<int> RandomlyRecreateAsync(string word)
+{
+    return Task.Run(() =>
+    {
+        int attempts = 0;
+        string randomWord;
+        Random random = new Random();
+
+        DateTime startTime = DateTime.Now;
+        do
+        {
+            char[] chars = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                chars[i] = (char)('a' + random.Next(26));
+            }
+            randomWord = new string(chars);
+            attempts++;
+        }
+        while (randomWord != word);
+        
+        DateTime endTime = DateTime.Now;
+        TimeSpan timeSpan = endTime - startTime;
+        Console.WriteLine($"The word '{word}' was generated after {attempts:N0} attempts in {timeSpan} seconds.");
+        
+        return attempts;
+    });
 }
