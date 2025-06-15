b9003c4a790c74287df84b73842bdc86caf1dcc4
packet-nova <ellis.derek@outlook.com>
2025-06-15 15:19:51 -0400

    52 progress

---
+++ Level52TheFinalBattle/Game.cs
@@ -0,0 +1,62 @@
+﻿public class Game
+{
+    private readonly List<ICombat> _playerParty;
+    private readonly List<ICombat> _monsterParty;
+
+    public List<ICombat> CurrentTurn { get; private set; }
+
+    public Game()
+    {
+        Console.Write("What is the True Programmer's name? ");
+        string trueProgrammer = Console.ReadLine();
+        TrueProgrammer player = new(trueProgrammer);
+        Skeleton s1 = new("SKELETON");
+
+        _playerParty = new List<ICombat>();
+        _monsterParty = new List<ICombat>();
+
+        _playerParty.Add(player);
+        _monsterParty.Add(s1);
+
+        CurrentTurn = _playerParty;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            if (CurrentTurn == _playerParty)
+            {
+                PlayerPartyTurn();
+            }
+            else
+            {
+                MonsterPartyTurn();
+            }
+
+            CurrentTurn = CurrentTurn == _playerParty ? _monsterParty : _playerParty;
+        }
+    }
+
+    public void PlayerPartyTurn()
+    {
+        foreach (var member in _playerParty)
+        {
+            Console.WriteLine($"It is {member.Name}'s turn.");
+            Console.Write("Action? ");
+            Console.ReadLine();
+            member.DoNothing();
+        }
+    }
+
+    public void MonsterPartyTurn()
+    {
+        foreach (var member in _monsterParty)
+        {
+            Console.WriteLine($"It is {member.Name}'s turn.");
+            Console.Write("Action? ");
+            Console.ReadLine();
+            member.DoNothing();
+        }
+    }
+}
--- Level52TheFinalBattle/Program.cs
+++ Level52TheFinalBattle/Program.cs
@@ -1,80 +1,29 @@
 ﻿Game game = new();
 game.Run();
 
-
-public class Game
-{
-    private readonly List<Skeleton> _playerParty;
-    private readonly List<Skeleton> _monsterParty;
-
-
-    public Game()
-    {
-        Skeleton s1 = new("SKELETON");
-        Skeleton s2 = new("SKELETON");
-        _playerParty = new List<Skeleton>();
-        _monsterParty = new List<Skeleton>();
-        _playerParty.Add(s1);
-        _monsterParty.Add(s2);
-    }
-    public void Run()
-    {
-        while (true)
-        {
-            Thread.Sleep(500);
-            Console.WriteLine($"It is SKELETON's turn.");
-            PlayerPartyTurn();
-            Thread.Sleep(500);
-            Console.WriteLine($"It is SKELETON's turn.");
-            MonsterPartyTurn();
-        }
-    }
-
-    public void PlayerPartyTurn()
-    {
-        foreach (var member in _playerParty)
-        {
-            Console.Write("Action? ");
-            Console.ReadLine();
-            member.DoNothing();
-        }
-    }
-    public void MonsterPartyTurn()
-    {
-        foreach (var member in _monsterParty)
-        {
-            Console.Write("Action? ");
-            Console.ReadLine();
-            member.DoNothing();
-        }
-    }
-
-
-    public void ClearParty<T>(List<T> list)
-    {
-        list.Clear();
-    }
-}
-
-public class Player
+public class TrueProgrammer : ICombat
 {
     public string Name { get; private set; }
 
-    public Player(string name)
+    public TrueProgrammer(string name)
     {
         Name = name;
     }
 }
 
-public class Skeleton
+public class Skeleton : ICombat
 {
     public string Name { get; init; }
 
     public Skeleton(string name)
     {
         Name = name;
-    }   
+    }
+}
 
+public interface ICombat
+{
+    public string Name { get; }
     public void DoNothing()
     {
         Console.WriteLine($"{Name} did NOTHING.");
