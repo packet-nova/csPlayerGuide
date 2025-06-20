9aeb051e622e707cadfc1a29198848412ea9e6f9
packet-nova <ellis.derek@outlook.com>
2025-06-20 16:57:06 -0400

    implemented basic attack

---
+++ Level52TheFinalBattle/Attack.cs
@@ -0,0 +1,7 @@
+﻿public class Attack : IBattleCommand
+{
+    public void Execute(IBattleEntity entity)
+    {
+        Console.WriteLine($"{entity.Name} attacked.");
+    }
+}
--- Level52TheFinalBattle/BattleCommandFactory.cs
+++ Level52TheFinalBattle/BattleCommandFactory.cs
@@ -3,6 +3,7 @@
     public static IBattleCommand PlayerCommand(BattleAction action) => action switch
     {
         BattleAction.DoNothing => new DoNothing(),
+        BattleAction.Attack => new Attack(),
         _ => new DoNothing()
     };
 }
