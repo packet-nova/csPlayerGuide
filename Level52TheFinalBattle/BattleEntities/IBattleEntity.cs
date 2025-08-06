5ef58381153961fece9e9ad74b127791576eadcf
packet-nova <ellis.derek@outlook.com>
2025-08-06 11:06:29 -0400

    Starting battle logger implementation

--- Level52TheFinalBattle/BattleEntities/IBattleEntity.cs
+++ Level52TheFinalBattle/BattleEntities/IBattleEntity.cs
@@ -4,9 +4,13 @@
     int MaxHP { get; }
     int CurrentHP { get; }
 
-/// <summary>
-/// Gets the collection of battle commands available for use in combat scenarios.
-/// </summary>
+    /// <summary>
+    /// Gets the collection of battle commands available for use in combat scenarios.
+    /// </summary>
     List<IBattleCommand> BattleCommands { get; }
+
+    /// <summary>
+    /// Reduces the health of the entity by the specified damage value.
+    /// </summary>
     void TakeDamage(int damageValue) { }
 }
