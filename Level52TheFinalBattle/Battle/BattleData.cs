046bb45efa05c88ed6d841cccf383cdbd11ff6fd
packet-nova <ellis.derek@outlook.com>
2025-08-25 09:55:39 -0400

    Basic BattleGenerator configured and using new BattleData record.

--- Level52TheFinalBattle/Battle/BattleData.cs
+++ Level52TheFinalBattle/Battle/BattleData.cs
@@ -1,7 +1,8 @@
 ﻿namespace Level52TheFinalBattle.Battle
 {
-    public record BattleData{
-        public BattleParty HeroParty { get; init; }
-        public BattleParty MonsterParty { get; }
+    public record BattleData
+    {
+        public required BattleParty HeroParty { get; set; }
+        public required BattleParty MonsterParty { get; set; }
     }
 }
--- Level52TheFinalBattle/Battle/BattleGenerator.cs
+++ Level52TheFinalBattle/Battle/BattleGenerator.cs
@@ -1,16 +1,40 @@
-﻿namespace Level52TheFinalBattle.Battle
+﻿using Level52TheFinalBattle.BattleEntities;
+
+namespace Level52TheFinalBattle.Battle
 {
     public class BattleGenerator
     {
+        private readonly Random rng = new();
 
-        public BattleData GenerateBattle(BattleParty heroParty, int battleTier)
+        public List<Monster> EasyMonsters { get; private set; } = [new Skeleton()];
+        public List<Monster> BossMonsters { get; private set; } = [new TheCodedOne()];
+
+        public BattleData GenerateBattle(
+            BattleParty heroParty,
+            Player heroController,
+            Player monsterController,
+            int battleTier)
         {
-            return new BattleData
+            List<IBattleEntity> monsters = battleTier switch
+            {
+                1 => [EasyMonsters[rng.Next(EasyMonsters.Count)]],
+                2 => [BossMonsters[rng.Next(BossMonsters.Count)]],
+                _ => throw new InvalidOperationException($"No battle implemented for battle tier: {battleTier}.")
+            };
+
+            var monsterParty = new BattleParty(monsters, monsterController);
+            
+            return new BattleData()
             {
                 HeroParty = heroParty,
-
+                MonsterParty = monsterParty,
             };
         }
 
+        public Monster RandomEasyMonster()
+        {
+            var monster = EasyMonsters.ElementAt(rng.Next(EasyMonsters.Count));
+            return monster;
+        }
     }
 }
