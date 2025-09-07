99dd48c715a5f14ec9577974de2a208f99fa5a49
packet-nova <ellis.derek@outlook.com>
2025-09-06 23:25:33 -0400

    Created VinFletcher character.

---
+++ Level52TheFinalBattle/BattleEntities/VinFletcher.cs
@@ -0,0 +1,15 @@
+﻿using Level52TheFinalBattle.BattleCommands;
+
+namespace Level52TheFinalBattle.BattleEntities
+{
+    public class VinFletcher : Character
+    {
+        public override string Name => "Vin Fletcher";
+        public override int MaxHP => 15;
+        
+        public VinFletcher()
+        {
+            BattleCommands = [new Punch()];
+        }
+    }
+}
