8a924d1cd666434980088e4923aa1298f03ec580
packet-nova <ellis.derek@outlook.com>
2025-09-09 15:03:47 -0400

    Removed IsEmpty property from BattleParty

--- Level52TheFinalBattle/Battle/BattleParty.cs
+++ Level52TheFinalBattle/Battle/BattleParty.cs
@@ -6,13 +6,8 @@ namespace Level52TheFinalBattle.Battle
     public class BattleParty
     {
         public List<IBattleEntity> Entities { get; }
-
         public Player Controller { get; }
-
-        public bool IsEmpty => Entities.Count == 0;
-
         public List<InventoryItem> Items { get; } = [];
-
         public BattleParty(List<IBattleEntity> entities, Player controller)
         {
             Entities = entities;
