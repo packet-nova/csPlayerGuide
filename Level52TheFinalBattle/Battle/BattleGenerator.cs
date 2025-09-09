09888f5e9c24267edc1f3419e8c597840792d775
packet-nova <ellis.derek@outlook.com>
2025-09-09 09:03:18 -0400

    Udpated battle generator logic to use a switch for monsters and also updated the method to take a quantity as parameters

--- Level52TheFinalBattle/Battle/BattleGenerator.cs
+++ Level52TheFinalBattle/Battle/BattleGenerator.cs
@@ -14,8 +14,8 @@ namespace Level52TheFinalBattle.Battle
         {
             List<IBattleEntity> monsters = battleTier switch
             {
-                1 => CreateEasyMonsters(),
-                2 => [.. CreateEasyMonsters(), .. CreateEasyMonsters()],
+                1 => CreateEasyMonster(rng.Next(1, 3)),
+                2 => CreateEasyMonster(rng.Next(2, 4)),
                 3 => [new TheUncodedOne()],
                 _ => throw new InvalidOperationException($"No battle implemented for battle tier: {battleTier}.")
             };
@@ -29,26 +29,33 @@ namespace Level52TheFinalBattle.Battle
             };
         }
 
-        public List<IBattleEntity> CreateEasyMonsters()
+        public List<IBattleEntity> CreateEasyMonster(int quantity)
         {
             List<IBattleEntity> monsters = [];
-            int count = rng.Next(1, 3);
-            Console.WriteLine($"DEBUG: Creating {count} monster(s)");
+            Console.WriteLine($"DEBUG: Creating {quantity} monster(s)");
 
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < quantity; i++)
             {
-                int m = rng.Next(1, 3);
+                int monsterChoice = rng.Next(0, 3);
 
-                if (m % 2 == 0)
+                switch (monsterChoice)
                 {
-                    monsters.Add(new Skeleton());
-                    Console.WriteLine($"DEBUG: Added Skeleton");
-                }
-
-                else
-                {
-                    monsters.Add(new StoneAmarok());
-                    Console.WriteLine($"DEBUG: Added Stone Amarok");
+                    case 0:
+                        monsters.Add(new Skeleton());
+                        Console.WriteLine($"DEBUG: Added Skeleton");
+                        break;
+                    case 1:
+                        monsters.Add(new StoneAmarok());
+                        Console.WriteLine($"DEBUG: Added Stone Amarok");
+                        break;
+                    case 2:
+                        monsters.Add(new Zombie());
+                        Console.WriteLine($"DEBUG: Added Zombie");
+                        break;
+                    default:
+                        monsters.Add(new Skeleton());
+                        Console.WriteLine($"DEBUG: OUT OF BOUNDS - Default to Skeleton");
+                        break;
                 }
             }
             Console.WriteLine("DEBUG: Press any key to continue...");
